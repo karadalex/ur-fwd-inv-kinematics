2bc1b9366b1131823533e3cce3ecc0fee56e74d3
karadalex <karadalex@gmail.com>
2019-04-29 21:03:10 +0300

    Solve successfully for all angles

--- invKinSym.m
+++ invKinSym.m
@@ -9,6 +9,8 @@ function angles = invKinSym(M_th, M_joints)
     known = symvar(M_symbols);
     
     % Get equations for th1, th5, th6
+    disp("Solving for th1, th5, th6 ...");
+    restart = false;
     for epochs = 1:1:3
         invM_1 = invTransf(M_joints(:,:,1));
         invM_6 = invTransf(M_joints(:,:,6));
@@ -17,7 +19,7 @@ function angles = invKinSym(M_th, M_joints)
         for j = 2:1:5
             rhs = rhs * M_joints(:,:,j);
         end
-        for j = 2:1:3
+        for j = 1:1:3
             for k = 1:1:4
                 eq = expand(lhs(j,k) - rhs(j,k));
                 eqVars = symvar(eq);
@@ -31,14 +33,50 @@ function angles = invKinSym(M_th, M_joints)
                     angles(char(unknown)) = eqSol;
                     disp(eqSol);
                     known = [known, unknown];
+                    restart = true;
+                    break;
                 end
                 eq_index = eq_index + 1;
-
+            end
+            if restart
+                restart = false;
+                break;
             end
         end
     end
     
+    % Get equations for th4
+    disp("Solving for th4 ...");
+    lhs = M_target;
+    for j = 1:1:2
+        lhs = invTransf(M_joints(:,:,j)) * lhs;
+    end
+    for j = 4:1:6
+        lhs = lhs * invTransf(M_joints(:,:,j));
+    end
+    rhs = M_joints(:,:,3);
+    for j = 1:1:3
+        for k = 1:1:4
+            eq = simplify(lhs(j,k) - rhs(j,k));
+            eqVars = symvar(eq);
+            unknown = setdiff(eqVars, known);
+            fprintf('EQ %d:   %s \n', eq_index, eq);
+            disp(unknown);
+            if length(unknown) == 1
+                eq = rewrite(eq, 'tan');
+                %eq = simplify(eq);
+                eqSol = solve(eq, unknown);
+                %eqSol = simplify(eqSol);
+                angles(char(unknown)) = eqSol;
+                disp(eqSol);
+                known = [known, unknown];
+            end
+            eq_index = eq_index + 1;
+        end
+    end
+    
     % Get equations for th2
+    disp("Solving for th2 ...");
     lhs = M_target;
     for j = 6:-1:4
         lhs = lhs * invTransf(M_joints(:,:,j));
@@ -47,6 +85,7 @@ function angles = invKinSym(M_th, M_joints)
     for j = 1:1:3
         rhs = rhs * M_joints(:,:,j);
     end
+    restart = false;
     for j = 1:1:3
         for k = 1:1:4
             eq = simplify(lhs(j,k) - rhs(j,k));
@@ -61,13 +100,23 @@ function angles = invKinSym(M_th, M_joints)
                 angles(char(unknown)) = eqSol;
                 disp(eqSol);
                 known = [known, unknown];
+                restart = true;
+                break;
             end
             eq_index = eq_index + 1;
         end
+        if restart
+            restart = false;
+            break;
+        end
     end
     
-    % Get equations for th3, th4
-    lhs = invTransf(M_joints(:,:,2)) * invTransf(M_joints(:,:,1)) * M_target;
+    % Get equations for th3
+    disp("Solving for th3 ...");
+    lhs = M_target;
+    for j = 1:1:2
+        lhs = invTransf(M_joints(:,:,j)) * lhs;
+    end
     rhs = sym(eye(4));
     for j = 3:1:6
         rhs = rhs * M_joints(:,:,j);
@@ -81,8 +130,9 @@ function angles = invKinSym(M_th, M_joints)
             disp(unknown);
             if length(unknown) == 1
                 eq = rewrite(eq, 'tan');
+                %eq = simplify(eq);
                 eqSol = solve(eq, unknown);
-                eqSol = simplify(eqSol);
+                %eqSol = simplify(eqSol);
                 angles(char(unknown)) = eqSol;
                 disp(eqSol);
                 known = [known, unknown];
--- inverse_script.m
+++ inverse_script.m
@@ -14,20 +14,28 @@ for angle = keys(solutions)
     solutions(key) = subs(solutions(key), sym('M', [3 4]), M_num(1:3, 1:4));
 end
 
+%% Substitute and evaluate solutions
+% Helper variables
 th = sym('th', [1, 6]);
-th1 = solutions(char(th(1)));
+th1 = eval(solutions(char(th(1))));
 th2 = solutions(char(th(2)));
-% th3 = solutions(char(th(3)));
-% th4 = solutions(char(th(4)));
+th3 = solutions(char(th(3)));
+th4 = solutions(char(th(4)));
 th5 = solutions(char(th(5)));
 th6 = solutions(char(th(6)));
-
+% substitute and evaluate and re-evaluate
+solutions(char(th(1))) = th1;
 solutions(char(th(5))) = eval(subs(th5, th(1), th1));
-solutions(char(th(6))) = eval(subs(th6, th(1), th1));
+solutions(char(th(6))) = eval(subs(th6, {th(1), th(5)}, {th1, th5}));
+solutions(char(th(4))) = eval(subs(th4, {th(1), th(5), th(6)}, {th1, th5, th6}));
+solutions(char(th(4))) = eval(solutions(char(th(4))));
 solutions(char(th(2))) = eval(subs(th2, {th(1), th(5), th(6)}, {th1, th5, th6}));
-% solutions(char(th(3))) = subs(th3, {th(5), th(6)}, {th5, th6});
-% solutions(char(th(4))) = subs(th4, {th(1), th(2), th(5), th(6)}, {th1 ,th2, th5, th6});
+solutions(char(th(2))) = eval(solutions(char(th(2))));
+solutions(char(th(3))) = eval(subs(th3, {th(1), th(2), th(4), th(5), th(6)}, {th1, th2, th4, th5, th6}));
+solutions(char(th(3))) = eval(solutions(char(th(3))));
+solutions(char(th(3))) = eval(solutions(char(th(3))));
 
+%% Display solutions
 for angle = keys(solutions)
     key = char(angle);
     disp(key);
@@ -35,5 +43,5 @@ for angle = keys(solutions)
 end
 
 %% Test results
-test_ik_angles = [];
-[test_ik_pos,test_ik_orient] = fwdKinPose(M, test_ik_angles(1,:));
+% test_ik_angles = [];
+% [test_ik_pos,test_ik_orient] = fwdKinPose(M, test_ik_angles(1,:));
