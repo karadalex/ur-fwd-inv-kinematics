f4f03cfaf4b8bddad866cf9a0fff3366b4e23885
karadalex <karadalex@gmail.com>
2019-04-29 21:02:10 +0300

    zeroed d1 for easier calculations and changed test angles

--- forward_script.m
+++ forward_script.m
@@ -1,7 +1,7 @@
 L = [0, 0.612, 0.572, 0, 0, 0];
-d = [0.128, 0, 0, 0.164, 0.116, 0.092];
+d = [0, 0, 0, 0.164, 0.116, 0.092];
 a = [0, -pi/2, 0, 0, pi/2, -pi/2];
 
 [M, M_joints] = fwdKinSym(L, d, a);
-test_angles = [0.8, pi, -pi, pi/2, pi/2, pi/4];
+test_angles = [0.2, 2.80, -3, 1.45, 0.65, 0.78];
 [pos, orient] = fwdKinPose(M, test_angles);
