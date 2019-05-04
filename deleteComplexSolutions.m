46ddd4ce3e294d593272fff5d117efd6052a1381
karadalex <karadalex@gmail.com>
2019-05-04 23:53:54 +0300

    reduce decimal point accuracy

--- deleteComplexSolutions.m
+++ deleteComplexSolutions.m
@@ -2,7 +2,7 @@ function realSol = deleteComplexSolutions(complexSol)
     realSol = [];
     for i = 1:1:length(complexSol)
         sol = complexSol(i);
-        if abs(imag(sol)) < 10^(-8)
+        if abs(imag(sol)) < 0.5*10^(-7)
             realSol = [realSol, real(sol)];
         end
     end
