ecb660bb4f2d014dd2d95ec78f1359388eb372d9
karadalex <karadalex@gmail.com>
2019-05-06 02:36:40 +0300

    solve IK by breaking the problem in 2 parts, one of which is a RRR robot problem

--- invKinSym.m
+++ invKinSym.m
@@ -3,8 +3,6 @@ function angles = invKinSym(M_joints)
     M_symbols = sym('M', [3 4]);
     M_target(1:3, 1:4) = M_symbols;
     angles = containers.Map;
-    
-    theta = sym('th', [1, 6]);
     known = symvar(M_symbols);
     
     % Solve for th1, th5, th6
@@ -18,29 +16,38 @@ function angles = invKinSym(M_joints)
     end
     [angles, known] = solveIKequations(lhs, rhs, known, angles, 3);
     
-    % Solve for th2
-    disp("Solving for th2 ...");
-    lhs = M_target;
-    for j = 1:1:2
-        lhs = invTransf(M_joints(:,:,j)) * lhs;
+    % Solve for th2, th3, th4 (RRR robot)
+    disp("Solving for th2, th3, th4 ...");
+    angles234 = invKinRRRSym(M_joints(:,:,2:4));
+    M_1_6 = invTransf(M_joints(:,:,1)) * M_target;
+    M_1_4 = M_1_6 * invTransf(M_joints(:,:,5) * M_joints(:,:,6));
+    for angle = keys(angles234)
+        key = char(angle);
+        angles234(key) = subs(angles234(key), sym('T', [3 4]), M_1_4(1:3, 1:4));
+        disp(angles234(key));
     end
-    for j = 6:-1:4
-        lhs = lhs * invTransf(M_joints(:,:,j));
-    end
-    rhs = M_joints(:,:,3);
-    [angles, known] = solveIKequations(lhs, rhs, known, angles, 1);
-    
-    % Solve for th4,th3
-    disp("Solving for th4, th3 ...");
-    lhs = M_target;
-    for j = 6:-1:4
-        lhs = lhs * invTransf(M_joints(:,:,j));
-    end
-    rhs = sym(eye(4));
-    for j = 1:1:3
-        rhs = rhs * M_joints(:,:,j);
-    end
-    [angles, known] = solveIKequations(lhs, rhs, known, angles, 2);    
+    angles = [angles; angles234];
+%     lhs = M_target;
+%     for j = 1:1:2
+%         lhs = invTransf(M_joints(:,:,j)) * lhs;
+%     end
+%     for j = 6:-1:4
+%         lhs = lhs * invTransf(M_joints(:,:,j));
+%     end
+%     rhs = M_joints(:,:,3);
+%     [angles, known] = solveIKequations(lhs, rhs, known, angles, 1);
+%     
+%     % Solve for th4,th3
+%     disp("Solving for th4, th3 ...");
+%     lhs = M_target;
+%     for j = 6:-1:4
+%         lhs = lhs * invTransf(M_joints(:,:,j));
+%     end
+%     rhs = sym(eye(4));
+%     for j = 1:1:3
+%         rhs = rhs * M_joints(:,:,j);
+%     end
+%     [angles, known] = solveIKequations(lhs, rhs, known, angles, 2);    
     
     disp(known);
     fprintf('\n');
