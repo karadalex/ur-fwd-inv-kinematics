307f36ea2e4808de4ea6e207fcec06506aff61b9
Alexios Karadimos <karadalex@gmail.com>
2020-06-19 14:59:40 +0300

    Calculate the Jacobian for each solution

--- ur10_kinematics.m
+++ ur10_kinematics.m
@@ -7,9 +7,11 @@ a = [pi/2, 0, 0, pi/2, -pi/2, 0];
 
 
 %% Inverse Kinematics
-M_U_0 = pose(0,0,0,0,0,0);
+M_U_0 = pose(-0.2664, -0.2750, -0.8, -pi/2, 0, 0);
+%M_U_0 = pose(0, 0, 0, 0, 0, 0);
 M_U_TCP = pose(0.2,0.1,0.5,pi,0,pi/2);
-M_6_TCP = pose(0,0,0,0,0,0);
+M_6_TCP = pose(0, 0, 0.1606, 0, 0, 0);
+%M_6_TCP = pose(0, 0, 0, 0, 0, 0);
 M_0_6 = inv(M_U_0) * M_U_TCP * inv(M_6_TCP);
 
 th = invKin(M_0_6, M_joints, L, d, a);
@@ -18,7 +20,16 @@ th = invKin(M_0_6, M_joints, L, d, a);
 %% Solutions validation
 for i=1:8
     solution = th(:,i)';
-    Mv = eval(subs(M, sym('th', [1, 6]), solution));
-    Mv_U_TCP = M_U_0 * Mv * M_6_TCP;
-    %disp(Mv_U_TCP - M_0_6);
+    M_test = eval(subs(M, sym('th', [1, 6]), solution));
+    M_test_U_TCP = M_U_0 * M_test * M_6_TCP;
+    disp(M_test_U_TCP - M_U_TCP);
+end
+
+%% Calculate the Jacobian
+disp("Jacobians:");
+for i=1:8
+    solution = th(:,i)';
+    M_test = eval(subs(M, sym('th', [1, 6]), solution));
+    Jv = geometricJacobianNum(solution, M, M_joints, 6);
+    disp(Jv);
 end
