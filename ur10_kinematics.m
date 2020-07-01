082fced673ee29fea18929b75227580a0274c31f
Alexios Karadimos <karadalex@gmail.com>
2020-07-01 12:44:13 +0300

    Symbolic Jacobian results to Latex

--- ur10_kinematics.m
+++ ur10_kinematics.m
@@ -35,3 +35,28 @@ for i=1:8
     Jv = geometricJacobianNum(solution, M, M_joints, 6);
     disp(Jv);
 end
+
+%% Symbolic Jacobian
+Jv_sym = geometricJacobian(M, M_joints, 6);
+th_sym = sym('th', [1, 6]);
+c_sym = sym('c', [1, 6]);
+s_sym = sym('s', [1, 6]);
+Jv_sym = subs(Jv_sym, conj(th_sym), th_sym);
+Jv_sym = subs(Jv_sym, [cos(th_sym), sin(th_sym)], [c_sym, s_sym]);
+th234 = sym('th234');
+Jv_sym = subs(Jv_sym, th_sym(2)+th_sym(3)+th_sym(4), th234);
+Jv_sym = subs(Jv_sym, [cos(th234), sin(th234)], [sym('c234'), sym('s234')]);
+th2345 = sym('th2345');
+Jv_sym = subs(Jv_sym, th234+th_sym(5), th2345);
+Jv_sym = subs(Jv_sym, [cos(th2345), sin(th2345)], [sym('c2345'), sym('s2345')]);
+th23 = sym('th23');
+Jv_sym = subs(Jv_sym, th_sym(2)+th_sym(3), th23);
+Jv_sym = subs(Jv_sym, [cos(th23), sin(th23)], [sym('c23'), sym('s23')]);
+
+Jvv = sym('Jv', [6 6]);
+% Convert to latex
+for i=1:6
+    for j=1:6
+        disp([latex(Jvv(i,j)), ' = ', latex(Jv_sym(i,j))]);
+    end
+end
